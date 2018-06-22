34500517b1814d7cccb341c93fc2493dfec46c47
OscarZhou <zhouhongyu1989@gmail.com>
2018-06-22 13:23:36 +1200

    Initialize the project

---
+++ request.go
@@ -0,0 +1,50 @@
+package Alidayu
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func DoRequest(method, url string, body []byte) (int, error) {
+	var reader io.Reader
+	switch method {
+	case "GET", "get":
+
+	case "POST", "post":
+		if len(body) == 0 {
+			return 411, errors.New("Length required: body. ")
+		}
+		reader = bytes.NewReader(body)
+	default:
+		return 400, errors.New("http method invalid. ")
+	}
+
+	req, err := http.NewRequest(method, url, reader)
+	if err != nil {
+		return 500, errors.New("Internal server error")
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if resp == nil {
+		return 500, errors.New("Internal server error. return null. ")
+	}
+
+	if err != nil {
+		return resp.StatusCode, errors.New(resp.Status)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode > 300 && resp.StatusCode < 200 {
+		return resp.StatusCode, errors.New(resp.Status)
+	}
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return resp.StatusCode, errors.New(resp.Status + "," + string(respBody))
+	}
+	return resp.StatusCode, nil
+}
---
+++ sms.go
@@ -0,0 +1,83 @@
+package Alidayu
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+	"time"
+)
+
+type SMS struct {
+	APPKey    string
+	APPSecret string
+	SMSParam  SMSParam
+}
+
+func NewSMS(config SMSConfig) (*SMS, error) {
+	if config.APPKey == "" || config.APPSecret == "" {
+		return nil, errors.New("key and secret can not be empty")
+	}
+
+	smsParam := SMSParam{
+		Type:           "normal",
+		FreeSignName:   config.FreeSignName,
+		Param:          config.Param,
+		ReceiverNumber: config.PhoneNumber,
+		TemplateCode:   config.TemplateCode,
+		Method:         "alibaba.aliqin.fc.sms.num.send",
+		APPKey:         config.APPKey,
+		TimeStamp:      time.Now().Format("2006-01-02 15:04:05"),
+		SignMethod:     "md5",
+		Format:         "json",
+		V:              "2.0",
+	}
+	sms := &SMS{
+		APPKey:    config.APPKey,
+		APPSecret: config.APPSecret,
+		SMSParam:  smsParam,
+	}
+	return sms, nil
+}
+
+func (sms *SMS) SendSMS() (int, error) {
+	url := "https://eco.taobao.com/router/rest"
+
+	body, err := sms.GetURLQuery()
+	if err != nil {
+		return 500, err
+	}
+
+	return DoRequest("POST", url, []byte(body))
+}
+
+// Sign generates the body that the http request needs
+func (sms *SMS) GetURLQuery() (string, error) {
+	var keys []string
+
+	k, err := ExtractNotNullKeys(sms.SMSParam)
+	if err != nil {
+		return "", err
+	}
+	keys = append(keys, k...)
+
+	keyMap := make(map[string]bool)
+	for _, v := range keys {
+		keyMap[v] = true
+	}
+
+	pMap, err := GenerateMap(sms.SMSParam, keyMap)
+	if err != nil {
+		return "", err
+	}
+	sms.SMSParam.Sign, err = SignTopRequest(pMap, sms.APPSecret, sms.SMSParam.SignMethod)
+	if err != nil {
+		return "", err
+	}
+
+	params := url.Values{}
+	params.Set("sign", strings.ToUpper(sms.SMSParam.Sign))
+	for k, v := range pMap {
+		params.Set(k, v)
+	}
+	return params.Encode(), nil
+}
